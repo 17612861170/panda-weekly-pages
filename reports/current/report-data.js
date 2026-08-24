058e1b09cdb7971978bf33472ae65602209b1be8
海霞 <zhx@haixiadeMacBook-Pro.local>
2026-08-24 19:05:17 +0800

    Patch Wuhan Huiju renew amount

--- reports/current/report-data.js
+++ reports/current/report-data.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var DATA_VERSION = '20260824-store-patch-v5';
+  var DATA_VERSION = '20260824-wuhanhuiju-renew-v6';
   var priorSupport = {};
   var hiddenSupportLabels = {
     '新签小卡数': true,
