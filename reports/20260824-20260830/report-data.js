37145d0919ea781b59fee4f5ad95a33c1567b283
海霞 <zhx@haixiadeMacBook-Pro.local>
2026-08-31 17:45:41 +0800

    Fill weekly card split metrics

--- reports/20260824-20260830/report-data.js
+++ reports/20260824-20260830/report-data.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var DATA_VERSION = '20260830-old-customers-v4';
+  var DATA_VERSION = '20260830-card-split-v5';
   var priorSupport = {};
   var hiddenSupportLabels = {
     '新签小卡数': true,
