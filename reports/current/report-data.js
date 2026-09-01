e4f45e49fed319be511637e681dc98bde1a5d380
海霞 <zhx@haixiadeMacBook-Pro.local>
2026-09-01 15:19:08 +0800

    Apply manual traffic corrections

--- reports/current/report-data.js
+++ reports/current/report-data.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var DATA_VERSION = '20260830-traffic-v14';
+  var DATA_VERSION = '20260830-manual-traffic-v15';
   var priorSupport = {};
   var hiddenSupportLabels = {
     '新签小卡数': true,
